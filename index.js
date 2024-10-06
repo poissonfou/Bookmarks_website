07bda570af6c564ff98f2b835830c018c4ea19af
Emerson Lima <emersonlimago123@gmail.com>
2024-10-06 19:26:56 -0300

    finished desktop

--- index.js
+++ index.js
@@ -42,57 +42,100 @@ tabList.addEventListener("keydown", (e) => {
   }
 });
 
-tabs.forEach((tab) =>
-  tab.addEventListener("click", (e) => {
-    const targetTab = e.target;
-    const targetPanel = targetTab.getAttribute("aria-controls");
-    const targetImg = targetTab.getAttribute("data-image");
-    const previousTab = document.querySelector('[aria-selected="true"]');
-    const previousPanel = previousTab.getAttribute("aria-controls");
-    const previousImg = previousTab.getAttribute("data-image");
+function selectTab(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImg = targetTab.getAttribute("data-image");
+  const previousTab = document.querySelector('[aria-selected="true"]');
+  const previousPanel = previousTab.getAttribute("aria-controls");
+  const previousImg = previousTab.getAttribute("data-image");
 
-    previousTab.setAttribute("aria-selected", false);
-    targetTab.setAttribute("aria-selected", true);
+  previousTab.setAttribute("aria-selected", false);
+  targetTab.setAttribute("aria-selected", true);
 
-    document.getElementById(previousPanel).setAttribute("hidden", true);
-    document.getElementById(targetPanel).removeAttribute("hidden");
+  document.getElementById(previousPanel).setAttribute("hidden", true);
+  document.getElementById(targetPanel).removeAttribute("hidden");
 
-    document.getElementById(previousImg).setAttribute("hidden", true);
-    document.getElementById(targetImg).removeAttribute("hidden");
-  })
-);
+  document.getElementById(previousImg).setAttribute("hidden", true);
+  document.getElementById(targetImg).removeAttribute("hidden");
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => selectTab(e));
+
+  tab.addEventListener("keydown", (e) => {
+    if (e.keyCode == 13) {
+      selectTab(e);
+    }
+  });
+});
 
 //accordion
 const tabsAccordion = document.querySelectorAll(".question__tab");
+const containerTabsAccordion =
+  document.getElementsByClassName("questions__tabs")[0];
+
+let tabFocusAccordion = 0;
+containerTabsAccordion.addEventListener("keydown", (e) => {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabsAccordion[tabFocusAccordion].setAttribute("tabindex", -1);
+
+    if (e.keyCode === keydownRight) {
+      tabFocusAccordion++;
+      if (tabFocusAccordion >= tabsAccordion.length) {
+        tabFocusAccordion = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocusAccordion--;
+      if (tabFocusAccordion < 0) {
+        tabFocusAccordion = tabsAccordion.length - 1;
+      }
+    }
+
+    tabsAccordion[tabFocusAccordion].setAttribute("tabindex", 0);
+    tabsAccordion[tabFocusAccordion].focus();
+  }
+});
+
+function openAccordion(tab) {
+  const tabSelected = tab.getAttribute("aria-selected");
+  const tabPanelId = tab.getAttribute("aria-controls");
+  const panel = document.getElementById(tabPanelId);
+
+  const previousTab = document.querySelector(
+    '.questions__tabs [aria-selected="true"]'
+  );
+
+  if (previousTab && tabSelected == "false") {
+    const tabPreviousPanelId = previousTab.getAttribute("aria-controls");
+    const previousPanel = document.getElementById(tabPreviousPanelId);
+    previousTab.setAttribute("aria-selected", false);
+    previousPanel.classList.remove("question__answer--selected");
+    previousPanel.style.maxHeight = null;
+  }
+
+  if (tabSelected == "true") {
+    tab.setAttribute("aria-selected", false);
+    panel.style.maxHeight = null;
+    panel.classList.remove("question__answer--selected");
+    return;
+  }
+
+  tab.setAttribute("aria-selected", true);
+  panel.style.maxHeight = panel.scrollHeight + 50 + "px";
+  panel.classList.add("question__answer--selected");
+}
 
 tabsAccordion.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const tabSelected = tab.getAttribute("aria-selected");
-    const tabPanelId = tab.getAttribute("aria-controls");
-    const panel = document.getElementById(tabPanelId);
+  tab.addEventListener("click", () => openAccordion(tab));
 
-    const previousTab = document.querySelector(
-      '.questions__tabs [aria-selected="true"]'
-    );
-
-    if (previousTab && tabSelected == "false") {
-      const tabPreviousPanelId = previousTab.getAttribute("aria-controls");
-      const previousPanel = document.getElementById(tabPreviousPanelId);
-      previousTab.setAttribute("aria-selected", false);
-      previousPanel.classList.remove("question__answer--selected");
-      previousPanel.style.maxHeight = null;
+  tab.addEventListener("keydown", (e) => {
+    if (e.keyCode == 13) {
+      openAccordion(tab);
     }
-
-    if (tabSelected == "true") {
-      tab.setAttribute("aria-selected", false);
-      panel.style.maxHeight = null;
-      panel.classList.remove("question__answer--selected");
-      return;
-    }
-
-    tab.setAttribute("aria-selected", true);
-    panel.style.maxHeight = panel.scrollHeight + 50 + "px";
-    panel.classList.add("question__answer--selected");
   });
 });
 
